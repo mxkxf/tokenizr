0b7b89c3767b0d282e9842ae456343b8c28338a4
Mike Francis <mikeffrancis@gmail.com>
2014-11-08 22:19:55 +0000

    Intial commit

---
+++ spec/TokenizrSpec.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace spec;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class TokenizrSpec extends ObjectBehavior
+{
+
+  function it_throws_an_exception_for_a_string_length()
+  {
+    $this->shouldThrow('\InvalidArgumentException')->during('generate', ['a naughty string']);
+  }
+
+
+  function it_throws_an_exception_for_a_bad_integer_length()
+  {
+    $this->shouldThrow('\InvalidArgumentException')->during('generate', [-999]);
+  }
+
+  
+  function it_returns_a_random_string()
+  {
+    $this->generate(5)->shouldBeString();
+  }
+
+
+  function it_doesnt_create_duplicate_tokens()
+  {
+    $this->setCharacters('ab');
+    $this->setExistingTokens(['aa', 'ab', 'ba']);
+    $this->generate(2)->shouldReturn('bb');
+  }
+
+
+  function it_sets_bad_characters()
+  {
+    $this->shouldThrow('\InvalidArgumentException')->during('setCharacters', [0]);
+  }
+
+
+  function it_sets_bad_existing_tokens()
+  {
+    $this->shouldThrow('\InvalidArgumentException')->during('setExistingTokens', ['a naughty string']);
+  }
+
+}
---
+++ src/Tokenizr.php
@@ -0,0 +1,124 @@
+<?php
+
+class Tokenizr
+{
+
+  /**
+   * Bank of characters to generate string from
+   * 
+   * @var string
+   */
+  private $characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+
+  /**
+   * Holder for an existing tokens to compare against
+   * 
+   * @var array
+   */
+  private $existingTokens = [];
+
+
+  /**
+   * Generate a new token
+   * 
+   * @param  integer $length
+   * @return string
+   */
+  public function generate($length = 5)
+  {
+    if (! is_int($length) or $length <= 0)
+    {
+      throw new \InvalidArgumentException("{$length} is not a valid length of token to generate");
+    }
+    $token = '';
+    for ($i = 0; $i < $length; $i ++)
+    {
+      $token .= $this->getRandomCharacter();
+    }
+    if ($this->doesTokenExist($token, $this->existingTokens))
+    {
+      return $this->generate($length);
+    }
+    return $token;
+  }
+
+
+  /**
+   * Get a random character from the bank
+   * 
+   * @return string
+   */
+  private function getRandomCharacter()
+  {
+    $characters       = $this->getCharacters();
+    $charactersLength = strlen($characters) - 1;
+    $randomElement    = rand(0, $charactersLength);
+    return $characters[$randomElement];
+  }
+
+
+  /**
+   * Check if the supplied token already exists
+   * 
+   * @param  string $token
+   * @return bool
+   */
+  public function doesTokenExist($token)
+  {
+    return (bool) (in_array($token, $this->getExistingTokens()));
+  }
+
+
+  /**
+   * Set a new bank of characters
+   * 
+   * @param string $characters
+   */
+  public function setCharacters($characters)
+  {
+    if (!is_string($characters) or $characters === '')
+    {
+      throw new \InvalidArgumentException("$characters should be a string");
+    }
+    $this->characters = $characters;
+  }
+
+
+  /**
+   * Get the bank of characters in use
+   * 
+   * @return string
+   */
+  public function getCharacters()
+  {
+    return $this->characters;
+  }
+
+
+  /**
+   * Set the existing tokens array
+   * 
+   * @param array $tokens
+   */
+  public function setExistingTokens($tokens)
+  {
+    if (! is_array($tokens))
+    {
+      throw new \InvalidArgumentException("$tokens should be an array");
+    }
+    $this->existingTokens = $tokens;
+  }
+
+
+  /**
+   * Get the existing tokens array
+   * 
+   * @return array
+   */
+  public function getExistingTokens()
+  {
+    return $this->existingTokens;
+  }
+
+}
